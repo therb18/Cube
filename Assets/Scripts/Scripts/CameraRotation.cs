984140c45932f5924f79702c9c88b809858241ac
therb18 <158738473+therb18@users.noreply.github.com>
2024-04-28 22:40:52 +0500

    Восстановил проект
    Надеюсь

---
+++ Assets/Scripts/Scripts/CameraRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRotation : MonoBehaviour
+{
+    public Transform CameraAxisTransform;
+
+    public float RotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+        CameraAxisTransform.localEulerAngles = new Vector3(CameraAxisTransform.localEulerAngles.x + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y"), 0, 0);
+    }
+}
