3b9578fff2fe9c4a8379697a986e26201ab2f28d
IKI-245 <kirillivanov_2009@mail.ru>
2024-05-08 23:21:15 +0300

    Пауза и перезапуск игры

---
+++ Assets/Scenes/Scripts/ContinueController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueController : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject Button;
+    public GameObject Button_Exit;
+    public GameObject Button_Restart;
+    public GameObject TXT_Advice;
+
+    public void Continue()
+    {
+        Panel.SetActive(false);
+        Button.SetActive(false);
+        Button_Exit.SetActive(false);
+        Button_Restart.SetActive(false);
+        TXT_Advice.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Scripts/Paused.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paused : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject Button;
+    public GameObject Button_Exit;
+    public GameObject Button_Restart;
+    public GameObject TXT_Advice;
+
+    public void Pause()
+    {
+        Button.SetActive(true);
+        Panel.SetActive(true);
+        Button_Exit.SetActive(true);
+        Button_Restart.SetActive(true);
+        TXT_Advice.SetActive(true);
+
+        Time.timeScale = 0;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+}
