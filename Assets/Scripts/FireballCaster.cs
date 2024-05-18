23d82c61e400c8fcba385a58ab3df07f9ee41978
Vasilii21 <158750973+Vasilii21@users.noreply.github.com>
2024-05-18 11:10:01 +0300

    сделал стрельбу

---
+++ Assets/Scripts/FireballCaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCaster : MonoBehaviour
+{
+    public GameObject fireballPrefab;
+    public Transform fireballSourceTransform;
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(fireballPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
+        }
+    }
+}
