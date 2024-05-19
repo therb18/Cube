b3c29223de4f7eafc908ed3e442eaf8890554987
IKI-245 <kirillivanov_2009@mail.ru>
2024-05-19 20:19:27 +0300

    Приследование

---
+++ Assets/Scenes/Scripts/EndGamePortal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGamePortal : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerController player = other.GetComponent<PlayerController>();
+
+        if (player != null)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/PlayerHealth1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth1 : MonoBehaviour
+{
+    public float value = 100;
+    public RectTransform valueRectTransform;
+    public GameObject gameplayUI;
+    public GameObject gameOverScreen;
+    public GameObject PauseButton;
+
+    private float _maxValue;
+
+    public void DealDamage(float damage)
+    {
+        value -= damage;
+        if (value <= 0)
+        {
+            PlayerisDead();
+        }
+        DrawHealthBar();
+    }
+    private void PlayerisDead()
+    {
+        PauseButton.SetActive(false);
+        gameplayUI.SetActive(false);
+        gameOverScreen.SetActive(true);
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<cameraRotation>().enabled = false;
+        GetComponent<CharacterController>().enabled = false;
+
+    }
+
+    private void DrawHealthBar() => valueRectTransform.anchorMax = new Vector2(value / _maxValue, 1);    // Start is called before the first frame update
+    void Start()
+    {
+        _maxValue = value;
+        DrawHealthBar();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
